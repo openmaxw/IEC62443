3f6d3189a27bc8ad2438676798876a10e6b6e7a0
max <max@example.com>
2026-06-03 16:41:06 +0800

    fix: update pages base path for renamed repo

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/IEC62443/',
+  base: '/IEC62443_Alignment/',
   plugins: [react()],
   server: {
     host: '0.0.0.0',
